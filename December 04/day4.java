a0c1d6253b136e008423f2ee9857aaa2e2c6d634
codejaaz24 <109551025+codejaaz24@users.noreply.github.com>
2023-12-08 00:11:06 +0530

    Update day4.java

--- December 04/day4.java
+++ December 04/day4.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+
+public class day4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int i=0,j=s.length()-1;
+        String res ="";
+        while (i<j){
+            //System.out.println(s.substring(i,j+1));
+            if(s.substring(i,j+1).equals(rev(s.substring(i,j+1)))){
+                res = s.substring(i,j+1);
+            }
+            i++;
+            j--;
+        }
+        res=res.length()<1 ?"Error":res;
+        System.out.println(res);
+    }
+    public static String rev(String s){
+        StringBuilder sb =  new StringBuilder(s);
+        return sb.reverse().toString();
+    }
+}
 
