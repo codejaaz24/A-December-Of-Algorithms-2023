9bbfdb3f7470c97d8d91cf3aa7465f13e4fbe4c0
codejaaz24 <109551025+codejaaz24@users.noreply.github.com>
2023-12-08 00:11:50 +0530

    Update day5.java

--- December 05/day5.java
+++ December 05/day5.java
@@ -1,1 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class day5 {
+    public static void main(String[] args) {
+        Scanner in  = new Scanner(System.in);
+        String s = in.nextLine();
+        String[] arr = s.split(" ");
+        System.out.println(Arrays.toString(arr));
+        int[] array = new int[arr.length];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            array[i] = Integer.parseInt(arr[i]);
+            sum+=array[i];
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println(sum);
+        int s2=0;
+        for(int x:array){
+            if(x>=sum/ array.length){
+                s2+=x;
+            }
+        }
+        System.out.println(s2);
+    }
+}
 
