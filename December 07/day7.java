b5cf675223eee13dcc76e81417e692d48a9781c8
codejaaz24 <109551025+codejaaz24@users.noreply.github.com>
2023-12-08 00:12:41 +0530

    Update day7.java

--- December 07/day7.java
+++ December 07/day7.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+
+public class day7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(rectangleInCircle(5, 9, 5));
+    }
+
+    private static boolean rectangleInCircle(int l, int b, int r) {
+        double diag = Math.sqrt((double) Math.pow(l,2)+(double) Math.pow(b,2));
+        double diam = r*2;
+        return diag<=diam?true:false;
+    }
+}
 
